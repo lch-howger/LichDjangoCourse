1b7cff0537cd812efb302b44520601b827021468
lch-howger <360000521@qq.com>
2019-12-05 23:19:09 +0000

    update my_products

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from products import models
+from products.models import Product
 
 
 # Create your views here.
@@ -11,6 +11,7 @@ def home_view(request):
         'my_number': 123,
         'my_list': [1, 3, 5, 6, 7, 8, 8, 6, 5],
         'my_html': '<h1>This is my html<h1>',
+        'my_products':Product.objects.all(),
     }
 
     return render(request, 'home.html', my_context)
