097192ef916775176a82936be55d8c68485d123e
lch-howger <360000521@qq.com>
2019-12-05 21:42:25 +0000

    update for item in html

--- pages/views.py
+++ pages/views.py
@@ -5,11 +5,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    my_context={
-        'my_text':'This is my text.',
-        'my_number': 123
+    my_context = {
+        'my_text': 'This is my text.',
+        'my_number': 123,
+        'my_list': [1, 3, 5, 6, 7, 8, 8, 6, 5]
     }
-    return render(request, 'home.html',my_context)
+    return render(request, 'home.html', my_context)
 
 
 def contact_view(*args, **kwargs):
